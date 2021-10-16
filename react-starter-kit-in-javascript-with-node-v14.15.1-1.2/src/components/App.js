e361d82c70b8379df3f8a1afd4594d780898b2d6
yoshihiro <y.kadono4218@gmail.com>
2021-10-16 22:58:46 +0900

    [WIP] input form

--- react-starter-kit-in-javascript-with-node-v14.15.1-1.2/src/components/App.js
+++ react-starter-kit-in-javascript-with-node-v14.15.1-1.2/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Button} from '@material-ui/core';
+import InputFormLocal from "./InputFormLocal";
+import InputFormRemote from "./InputFormRemote";
 
 const getMedia = async () => {
   const constraints = {audio: true, video: true};
@@ -14,7 +15,12 @@ const getMedia = async () => {
 // getMedia();
 
 const App = () => {
-  return <Button color={"primary"} variant={"contained"}>Hello, React!</Button>;
+  return (
+    <>
+      <InputFormLocal />
+      <InputFormRemote />
+    </>
+  );
 }
 
 export default App;
---
+++ react-starter-kit-in-javascript-with-node-v14.15.1-1.2/src/components/InputFormLocal.js
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import {ThemeProvider} from '@material-ui/core/styles';
+import {createMuiTheme} from "@material-ui/core";
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/Yoshihiro4218">
+        Yoshi's github.
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createMuiTheme();
+
+export default function InputFormLocal() {
+  const label = 'あなたの名前';
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline/>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            {label}を入力してください
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+            <TextField
+              variant={"outlined"}
+              margin="normal"
+              required
+              fullWidth
+              label={label}
+              name="name"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{mt: 3, mb: 2}}
+            >
+              決定
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{mt: 8, mb: 4}}/>
+      </Container>
+    </ThemeProvider>
+  );
+}
